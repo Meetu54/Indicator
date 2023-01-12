5bda660c4ac692c11eab6cf56649b250b9500191
Meet Savaliya <89479235+Meetu54@users.noreply.github.com>
2023-01-12 04:16:39 -0800

    Add files via upload

---
+++ C program to reverse an array using pointers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+main () {
+	
+	int i;
+	int a[100];
+	int *b[100];
+	
+		b[100]=&a[100];
+		
+	printf(" enter the value : ");
+	scanf("%s",&a);
+	
+
+	
+	printf("%s",strrev(a));
+			
+			printf("%s",*b);
+		
+}
+
+
